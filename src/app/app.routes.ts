693e1469997379de777b4abf73d369dd5da5097b
112796-Agustin-Abraham-Loutaif <112796@tecnicatura.frc.utn.edu.ar>
2024-11-07 20:34:04 -0300

    Componentes y routing basico inplementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { NewOrderComponent } from './new-order/new-order.component';
+import { ListOrderComponent } from './list-order/list-order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "/create-order",
+        pathMatch: 'full'
+    },
+    {
+        path: "/create-order",
+        component: NewOrderComponent
+    },
+    {
+        path: "/orders",
+        component: ListOrderComponent
+    }
+];
